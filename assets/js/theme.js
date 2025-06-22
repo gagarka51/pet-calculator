432c554c39283ab991f559824aa5b4fd964062fb
Svetlana Sharkova <sharkova-2000@mail.ru>
2025-06-22 20:24:38 +0300

    add variables on theme.js

---
+++ assets/js/theme.js
@@ -0,0 +1,7 @@
+let calculator = document.getElementById("calculator");
+let body = document.body;
+let btn = document.getElementById("btn-theme");
+
+btn.onclick = function(){
+	alert("Смена темы");
+}
