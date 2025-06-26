6a8acc192e30636459fd520814f5215e9bb3a368
Svetlana Sharkova <sharkova-2000@mail.ru>
2025-06-26 16:34:09 +0300

    add styles of btn-img and btn-theme for night mode

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,11 +1,12 @@
 let calculator = document.getElementById("calculator");
 let body = document.body;
-//let display = document.getElementById("display");
 let btn = document.getElementById("btn-theme");
+let btnImg = document.getElementById("btn-theme-img");
 
 btn.onclick = function(){
 	body.style.backgroundColor = "#161a1f";
 	calculator.style.backgroundColor = "#020317";
 	btn.style.backgroundColor = "#020317";
 	display.style.color = "#fff";
+	btnImg.src = "assets/img/morning.svg";
 }
