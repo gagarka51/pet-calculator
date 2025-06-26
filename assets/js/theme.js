4826e95d6df78529226f1b516e08bc92c174bc41
Svetlana Sharkova <sharkova-2000@mail.ru>
2025-06-26 16:29:22 +0300

    edit old styles and add styles for night mode

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,7 +1,11 @@
 let calculator = document.getElementById("calculator");
 let body = document.body;
+//let display = document.getElementById("display");
 let btn = document.getElementById("btn-theme");
 
 btn.onclick = function(){
-	document.body.style.backgroundColor = "#161a1f";
+	body.style.backgroundColor = "#161a1f";
+	calculator.style.backgroundColor = "#020317";
+	btn.style.backgroundColor = "#020317";
+	display.style.color = "#fff";
 }
