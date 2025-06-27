1135537a6b33f45767ad1ce160e932c3ef3f7a72
Svetlana Sharkova <sharkova-2000@mail.ru>
2025-06-27 21:04:55 +0300

    edit theme.js(write function for change theme)

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -4,15 +4,24 @@ let btn = document.getElementById("btn-theme");
 let btnImg = document.getElementById("btn-theme-img");
 
 btn.onclick = function(){
+	addNightStyles();
+	if (body.classList.contains("night-body") == true) {
+		localStorage.setItem("theme", "light");
+	}
+	console.log(localStorage.getItem("theme"));
+}
+
+function addNightStyles() {
 	body.classList.toggle("night-body");
 	calculator.classList.toggle("night-calc");
 	btn.classList.toggle("night-btn");
 	display.classList.toggle("night-display");
 	btnImg.classList.toggle("night-img");
-	
+
 	if (btnImg.classList.contains("night-img") == true) {
 		btnImg.src = "assets/img/morning.svg";
 	} else {
 		btnImg.src = "assets/img/night-sky.svg";
 	}
+	localStorage.setItem("theme", "night");
 }
